9763c04d95b8ba4abc177954602376dba0138425
Hakuna20Matata20 <yarik.osolihin@gmail.com>
2025-07-18 14:54:16 +0200

    Second Commit from PySharm

--- app.py
+++ app.py
@@ -1,20 +1,14 @@
 from flask import Flask, request, jsonify
+import awsgi
 import random
-import awsgi  # <--- для запуску у AWS Lambda
 
 app = Flask(__name__)
 
-@app.route("/weather", methods=["GET"])
-def weather():
+@app.route("/weather")
+def get_weather():
     city = request.args.get("city", "Unknown")
-    temperature = f"{random.randint(10, 35)}°C"
-    condition = random.choice(["Sunny", "Cloudy", "Rainy", "Windy"])
-    return jsonify({
-        "city": city,
-        "temperature": temperature,
-        "condition": condition
-    })
+    temperature = f"{random.randint(15, 30)}°C"
+    return jsonify({"city": city, "temperature": temperature})
 
-# Вхідна точка для AWS Lambda
 def lambda_handler(event, context):
     return awsgi.response(app, event, context)
